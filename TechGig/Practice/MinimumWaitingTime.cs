720510a6b4f97f4c3d51f9acffa6c6e637938b75
uharishdhoni1991 <uharishdhoni1991@outlook.com>
2023-03-29 19:39:57 +0530

    Minimum Waiting time - Get the minimum waiting time for queries to execute together ! First query - 0 secs Second query - First Query's execution time Third query - First + Second query's combined execution time

---
+++ TechGig/Practice/MinimumWaitingTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace TechGig.Practice
+{
+    public class MinimumWaitingTime : IExecute
+    {
+        public void Execute()
+        {
+            int[] input = new int[] { 3, 2, 1, 2, 6 };
+            int mwt = GetMinimumWaitingTime(input);
+        }
+
+        public int GetMinimumWaitingTime(int[] input)
+        {
+            /*Hint 
+            (n - 1)i + (n - 2)(i + 1) + (n - 3)(i + 2) + (n - 4)(i + 3)*/
+
+            int mwt = 0;
+            int multiplier = input.Length - 1;
+
+            Array.Sort(input);
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                mwt += (input[i] * multiplier);
+                --multiplier;
+            }
+
+            return mwt;
+        }
+    }
+}
