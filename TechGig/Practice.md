ab4670f0f8c9a92f485c499c0537467bca345b13
uharishdhoni1991 <uharishdhoni1991@outlook.com>
2023-04-07 16:23:09 +0530

    Class Photo problem

--- TechGig/Practice.md
+++ TechGig/Practice.md
@@ -7,3 +7,9 @@ Tandem Bicycle
 
 Given : Speeds of riders as two lists - Blue and Red. Tandem cycle is operated by a pair of Blue and Red and the maximum speed rider controls the overall speed.
 Expected : Based on a input toggle find the minimum and maximum speed at which the bicycle can be operated.
+
+Class Photos
+
+Given : Heights of two sets of students wearing blue and red t shirts are given.
+Expected : Need to find out if u can arrange them in two rows to take a photo such that all the red 
+    		are in same row and as well as blue , and also the one in upper row should be taller than the one in below row.
---
+++ TechGig/Practice/ClassPhotos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TechGig.Practice
+{
+    public class ClassPhotos : IExecute
+    {
+        public void Execute()
+        {
+            bool isFeasible = CheckForPhotoArrangementFeasibility(
+                redShirts: new int[] { 5, 8, 1, 3, 4 },
+                blueShirts: new int[] { 6, 9, 2, 3, 5 });
+        }
+
+        public bool CheckForPhotoArrangementFeasibility(int[] redShirts, int[] blueShirts)
+        {
+            bool isFeasible = true;
+
+            Array.Sort(redShirts);
+            Array.Sort(blueShirts);
+
+            bool isRedTaller = redShirts[0] > blueShirts[0];
+
+            for(int i = 0; i < redShirts.Length; i++)
+            {
+                if(isRedTaller && isFeasible)
+                {
+                   isFeasible = isFeasible && redShirts[i] > blueShirts[i];
+                }
+                else if(isFeasible)
+                {
+                    isFeasible = isFeasible && blueShirts[i] > redShirts[i];
+                }
+                else
+                {
+                    isFeasible = false;
+                    return isFeasible;
+                }
+            }
+
+            return isFeasible;
+        }
+    }
+}
